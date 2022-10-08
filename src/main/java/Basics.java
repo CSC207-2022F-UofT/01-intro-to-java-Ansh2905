bbbcff55f9b3479f88bdbfe8c58e492ba01db639
Ansh2905 <anshdpatel10@gmail.com>
2022-10-07 20:59:23 -0400

    Completed assignment

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -7,6 +7,8 @@
  * relevant readings for each task.
  */
 
+import java.sql.SQLOutput;
+
 /**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
@@ -40,9 +42,7 @@ public class Basics {
          */
         System.out.println(7 + 5);
 
-        /* TODO (Task 1): Write a line of code below that prints the string
-         *                Hello World!
-         */
+        System.out.println("Hello World!");
 
 
 
@@ -58,10 +58,7 @@ public class Basics {
          *    (Relevant reading: 1.2. Variables and Types)
          */
 
-        /*
-         * TODO (Task 2): Create a variable named my_variable and assign it the
-         *                value 100.
-         */
+        int my_variable = 100;
 
 
 
@@ -100,6 +97,10 @@ public class Basics {
          * Current count: 0
          */
 
+        for(int i = 10; i>=0; i--)
+        {
+            System.out.println("Current count: " + i);
+        }
 
     }
 
@@ -141,7 +142,11 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
-
+        String[] to_split_array = to_split.split(" ");
+        for(int i = 0; i < to_split_array.length; i++)
+        {
+            ret.append(to_split_array[i].charAt(0));
+        }
         // Fill in the rest of the body here
 
         return ret.toString();
@@ -170,6 +175,13 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        if(arr.length != 0 && arr.length != 1)
+        {
+            for(int i=1; i< arr.length; i = i+2)
+            {
+                current_sum = current_sum + arr[i];
+            }
+        }
 
         return current_sum;
     }
